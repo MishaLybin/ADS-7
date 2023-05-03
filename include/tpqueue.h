f704edda00c5919e7b2e4076a2c5a7ac50d8accd
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-05-03 19:37:11 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -3,7 +3,7 @@
 #define INCLUDE_TPQUEUE_H_
 template<typename T>
 class TPQueue {
-   private:
+ private:
     struct Item {
         T data;
         Item* next = nullptr;
@@ -24,45 +24,46 @@ class TPQueue {
             head = create(value);
         }
     }
-    public:
-    TPQueue(): head(nullptr) {}
-    void push(const T& value) {
-        if (!head) {
-            addHead(value);
-        } else if ((*head).data.prior < value.prior) {
-            addHead(value);
-        } else {
-            Item* temp = head;
-            while (temp) {
-                if ((*temp).data.prior > value.prior) {
-                    temp = temp->next;
-                } else if ((*temp).data.prior == value.prior) {
-                    Item* temp_new = create(value);
-                    temp_new->next = temp->next;
-                    temp->next = temp_new;
-                    break;
-                } else {
-                    Item* temp_new = create((* temp).data);
-                    temp_new->next = temp->next;
-                    (* temp).data = value;
-                    temp->next = temp_new;
-                    break;
-                }
+
+ public:
+   TPQueue(): head(nullptr) {}
+   void push(const T& value) {
+      if (!head) {
+         addHead(value);
+      } else if ((*head).data.prior < value.prior) {
+         addHead(value);
+      } else {
+         Item* temp = head;
+         while (temp) {
+            if ((*temp).data.prior > value.prior) {
+               temp = temp->next;
+            } else if ((*temp).data.prior == value.prior) {
+               Item* temp_new = create(value);
+               temp_new->next = temp->next;
+               temp->next = temp_new;
+               break;
+            } else {
+               Item* temp_new = create((* temp).data);
+               temp_new->next = temp->next;
+               (* temp).data = value;
+               temp->next = temp_new;
+               break;
             }
-        }
-    }
-    const T pop() {
-        T data;
-        if (!(head->next)) {
-            data = head->data;
-        } else {
-            Item* temp = head->next;
-            data = head->data;
-            delete head;
-            head = temp;
-        }
-        return data;
-    }
+         }
+      }
+   }
+   const T pop() {
+      T data;
+      if (!(head->next)) {
+         data = head->data;
+      } else {
+         Item* temp = head->next;
+         data = head->data;
+         delete head;
+         head = temp;
+      }
+      return data;
+   }
 };
 struct SYM {
   char ch;
